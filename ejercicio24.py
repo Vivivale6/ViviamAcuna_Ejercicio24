b7818ffc355e6a654f09ee84421b480bfb219f7f
Vivivale6 <37335436+Vivivale6@users.noreply.github.com>
2018-05-03 11:46:43 -0500

    Update ejercicio24.py

--- ejercicio24.py
+++ ejercicio24.py
@@ -1,8 +1,29 @@
 import numpy as np
 import  matplotlib.pyplot as plt
+
 medicion= [1.2, 2.5,2.8,5.0]
+x= np.lispace(0.0001,100, 100)
+y= np.ones(100)
 
 
-def fun(x):
-    lamda=1
-    return 1/lamda*np.exp(-x/lamda)
+def fun(x, lamda):
+    
+    a = np.exp(-1.0/lamda)
+    b= np.exp(-20/lamda)
+    
+    return np.exp(-x/lamda)/(lamda*(a-b))
+c= len(medicion)
+
+def cuenta(y,c,x):
+    contador=0.0
+    for i in range(1000):
+        for j in range(c):
+            y[i]= y[i]*(fun[j], x[i])
+            
+        contador+=y[i]
+    return contador
+
+d= cuenta(y,c,x)
+y=y/d
+
+plt.plot(x,y)
